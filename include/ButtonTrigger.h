cd70da507937c7d7ac16b1c008d3b25bc3554676
Gary Gregory Rogers <gagregrog@gmail.com>
2026-02-22 17:26:13 -0800

    feat: debounce

--- include/ButtonTrigger.h
+++ include/ButtonTrigger.h
@@ -3,12 +3,17 @@
 
 class ButtonTrigger {
 public:
-  explicit ButtonTrigger(uint8_t pin, bool activeLow = true);
+  explicit ButtonTrigger(uint8_t pin, bool activeLow = true,
+                         unsigned long debounceMs = 50);
 
   void begin();
-  bool isPressed() const;
+  bool isPressed();
 
 private:
   uint8_t _pin;
   bool _activeLow;
+  unsigned long _debounceMs;
+  unsigned long _lastChangeTime;
+  bool _lastRaw;
+  bool _stable;
 };
--- src/ButtonTrigger.cpp
+++ src/ButtonTrigger.cpp
@@ -1,12 +1,27 @@
 #include "ButtonTrigger.h"
 
-ButtonTrigger::ButtonTrigger(uint8_t pin, bool activeLow)
-    : _pin(pin), _activeLow(activeLow) {}
+ButtonTrigger::ButtonTrigger(uint8_t pin, bool activeLow,
+                             unsigned long debounceMs)
+    : _pin(pin), _activeLow(activeLow), _debounceMs(debounceMs),
+      _lastChangeTime(0), _lastRaw(false), _stable(false) {}
 
 void ButtonTrigger::begin() {
   pinMode(_pin, _activeLow ? INPUT_PULLUP : INPUT);
+  _stable = digitalRead(_pin) == (_activeLow ? LOW : HIGH);
+  _lastRaw = _stable;
 }
 
-bool ButtonTrigger::isPressed() const {
-  return digitalRead(_pin) == (_activeLow ? LOW : HIGH);
+bool ButtonTrigger::isPressed() {
+  bool raw = digitalRead(_pin) == (_activeLow ? LOW : HIGH);
+
+  if (raw != _lastRaw) {
+    _lastChangeTime = millis();
+    _lastRaw = raw;
+  }
+
+  if ((millis() - _lastChangeTime) >= _debounceMs) {
+    _stable = _lastRaw;
+  }
+
+  return _stable;
 }
