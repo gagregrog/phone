453c58d6de747f05141a24cf3f37be6bb8cefb45
Gary Gregory Rogers <gagregrog@gmail.com>
2026-02-27 08:52:48 -0800

    fix: log handset events

--- src/hardware/HandsetEvents.cpp
+++ src/hardware/HandsetEvents.cpp
@@ -1,10 +1,12 @@
 #include "hardware/HandsetEvents.h"
 #include "hardware/HandsetJSON.h"
 #include "web/Events.h"
+#include "system/Logger.h"
 #include <ArduinoJson.h>
 
 void handsetEventsBegin(HandsetMonitor& handset) {
     handset.setOnChange([](bool offHook) {
+        logger.infof("Handset: %s", offHook ? "lifted" : "returned");
         JsonDocument doc;
         handsetFillJson(doc.to<JsonObject>(), offHook);
         String body;
